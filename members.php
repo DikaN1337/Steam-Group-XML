f2060b684bdb54b0bf5c711e3da56f90ecf05d6b
DikaN <diogomiguelcandido@gmail.com>
2019-08-16 12:40:03 +0100

    Enabled CURL support for this script.

--- members.php
+++ members.php
@@ -1,13 +1,30 @@
 <?php
 
-$steamgroup = "https://steamcommunity.com/groups/TESTE";
+$curl = true; // Use this if your hosting provider doesn't have allow_url_fopen enabled.
+$steamgroup = "TEST"; // Custom Grupo URL
 
-$url = $steamgroup."/memberslistxml/?xml=1";
-$xml = simplexml_load_file($url);
+$url = "https://steamcommunity.com/groups/" . $steamgroup . "/memberslistxml/?xml=1";
+
+if (curl = true) {
+  $ch = curl_init();
+  curl_setopt($ch,CURLOPT_URL,$url);
+  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,2);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $data = curl_exec($ch);
+  $xml = simplexml_load_string($data);
+  $json = json_encode($data);
+  $array = json_decode($json,TRUE);
+  curl_close($ch);
+} else if (curl = false) {
+  $xml = simplexml_load_file($url);
+} else {
+  die("Invalid Configuration.");
+}
+
 $avatarfull = $xml->groupDetails->avatarFull;
 $groupname = $xml->groupDetails->groupName;
 $members = $xml->groupDetails->memberCount;
-$groupurl = $xml->groupDetails->groupURL;
 
 echo "$members members in $groupname"; // ECHO MEMBERS AND GROUP NAME
 
